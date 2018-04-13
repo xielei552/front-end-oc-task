918b5fc23ad9a2ccac0165faf7a14952172183ec
Lei Xie <xielei552@gmail.com>
2018-04-13 17:28:26 -0400

    Setup an example Calendar table view

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+import Calendar from '../Calendar';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Calendar App</h1>
-        </header>
- 
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Calendar App</h1>
+          </header>
+          <Calendar />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
